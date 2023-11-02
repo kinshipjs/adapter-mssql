11937064d37572e66ec8f2c2ead6cdc342d8bf1f
traviszuleger <traviszuleger@yahoo.com>
2023-11-02 10:19:17 -0500

    fix datestring

--- src/index.js
+++ src/index.js
@@ -102,7 +102,7 @@ function escape(tableOrColumn, ignoreSchema=false) {
 function getDateString(date) {
     const year = date.getUTCFullYear().toString().padStart(4, '0');
     const month = (date.getUTCMonth()+1).toString().padStart(2, '0');
-    const day = (date.getUTCDate()+1).toString().padStart(2, '0');
+    const day = (date.getUTCDate()).toString().padStart(2, '0');
     const hours = date.getUTCHours().toString().padStart(2, '0');
     const mins = date.getUTCMinutes().toString().padStart(2, '0');
     return `${year}-${month}-${day} ${hours}:${mins}`;
